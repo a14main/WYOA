c322bf854b91e268a89de96d7af86ff9d2e99dd0
Andrew Quartermain <quartermain.andrew@gmail.com>
2024-10-15 04:07:11 +0800

    Implement register, login, and logout

---
+++ application/__init__.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, render_template, request, redirect
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
+    )
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+        
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/")
+    def index():
+        return render_template("hello.html")
+
+    @app.route("/login", methods=["GET", "POST"])
+    def login():
+        if request.method == "POST":
+            return redirect("/")
+        return render_template("login.html")
+    
+    
+    from . import db
+    db.init_app(app)
+    
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
+    return app
